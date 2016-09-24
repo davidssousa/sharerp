21b14ed93bd84f5912b612b37d76a7760ca64fa7
davidssousa <david.sousa@msn.com>
2016-09-24 20:13:02 -0300

    atualizando pasta node_models que subiu com publicacao

---
+++ server.js
@@ -0,0 +1,15 @@
+var http = require('http');
+
+var finalhandler = require('finalhandler');
+var serveStatic = require('serve-static');
+
+var serve = serveStatic("./public");
+
+var server = http.createServer(function(req, res) {
+  var done = finalhandler(req, res);
+  serve(req, res, done);
+});
+
+var port = Number(process.env.PORT || 3000);
+
+server.listen(port);
